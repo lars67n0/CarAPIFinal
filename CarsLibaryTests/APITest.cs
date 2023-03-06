64f74f1961bd56ab1962ab5bf69e19f77da0543a
lars67n0 <lars67n0@edu.zealand.dk>
2023-03-06 20:58:53 +0100

    Test

---
+++ CarsLibaryTests/APITest.cs
@@ -0,0 +1,24 @@
+﻿using CarsLibary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarAPIFinal;
+using Microsoft.AspNetCore.Routing;
+using CarAPIFinal.Controllers;
+
+
+namespace CarsLibaryTests
+{
+
+    [TestClass()]
+    public class APITest
+    {
+       
+
+    }
+
+    
+}
--- CarsLibaryTests/CarTests.cs
+++ CarsLibaryTests/CarTests.cs
@@ -11,29 +11,14 @@ namespace CarsLibary.Tests
     [TestClass()]
     public class CarTests
     {
-        //private Car c1;
-
-        //[TestInitialize] 
-        //public void TestIn()
-        //{ c1 = new Car("Honda", "501412", 5000); }
-
-        //[TestMethod]
-        //public void CarPriceTest()
-        //{
-        //    Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Price = 0);
-        //    Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Price = 0);
-
-        //    c1.Price = 10;
-
-        //    Assert.AreEqual(10, c1.Price);
-        //}
+        // Model Length Val
 
         [TestMethod]
         public void Model_SetWithValidValue_ShouldSetModel()
         {
             // Arrange
             var validModel = "ABCD";
-            var sut = new Car(); // Assuming the code is within a class called MyClass
+            var sut = new Car(); 
 
             // Act
             sut.Model = validModel;
@@ -42,42 +27,46 @@ namespace CarsLibary.Tests
             Assert.AreEqual(validModel, sut.Model);
         }
 
-        [TestMethod]
-        public void Model_SetWithNullValue_ShouldThrowArgumentException()
-        {
-            // Arrange
-            string nullModel = null;
-            var sut = new Car(); // Assuming the code is within a class called MyClass
+        
 
-            // Act & Assert
-            var ex = Assert.ThrowsException<ArgumentException>(() => sut.Model = nullModel);
-            Assert.AreEqual("Model must be at least 4 characters.", ex.Message);
-        }
-
-        [TestMethod]
-        public void Model_SetWithEmptyValue_ShouldThrowArgumentException()
-        {
-            // Arrange
-            var emptyModel = string.Empty;
-            var sut = new Car(); // Assuming the code is within a class called MyClass
-
-            // Act & Assert
-            var ex = Assert.ThrowsException<ArgumentException>(() => sut.Model = emptyModel);
-            Assert.AreEqual("Model must be at least 4 characters.", ex.Message);
-        }
+        
 
         [TestMethod]
         public void Model_SetWithShortValue_ShouldThrowArgumentException()
         {
             // Arrange
-            var shortModel = "ABSD";
-            var sut = new Car(); // Assuming the code is within a class called MyClass
+            var shortModel = "ABS";
+            var sut = new Car(); 
 
             // Act & Assert
             var ex = Assert.ThrowsException<ArgumentException>(() => sut.Model = shortModel);
             Assert.AreEqual("Model must be at least 4 characters.", ex.Message);
         }
 
+        // Price
+        [TestMethod]
+        public void PriceProperty_SettingInvalidValue_ThrowsArgumentException()
+        {
+            // Arrange
+            var product = new Car();
+            int invalidPrice = 0;
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => product.Price = invalidPrice);
+        }
+
+
+        //LicensePlate
+        [TestMethod]
+        public void LicensePlateProperty_SettingInvalidLength_ThrowsArgumentException()
+        {
+            // Arrange
+            var car = new Car();
+            string invalidLicensePlate = "";
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => car.LicensePlate = invalidLicensePlate);
+        }
 
     }
 }
